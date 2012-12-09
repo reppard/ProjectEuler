8fdcd91d6e00ea27af310daf2dfb3271874f47b9
reppardwalker <rwalker@reppardwalkers-MacBook-Air.local>
2012-12-09 14:54:50 -0500

    Problem 21 - It works now.

--- problem_21.rb
+++ problem_21.rb
@@ -1,7 +1,7 @@
 # Let d(n) be defined as the sum of proper divisors of n (numbers less than n which
 # divide evenly into n).
 
-# If d(a) = b and d(b) = a, where a  b, then a and b are an amicable pair and each
+# If d(a) = b and d(b) = a, where a !=  b, then a and b are an amicable pair and each
 # of a and b are called amicable numbers.
 
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110;
@@ -9,16 +9,17 @@
 
 # Evaluate the sum of all the amicable numbers under 10000.
 
+time = Time.now
+
 def d(n)
-  if !n.nil?
     (1..n).select{|i| i if n % i == 0 && i != n}.inject(&:+)
-  end
 end
 
 def has_pair?(n)
   a = d(n)
   b = d(a)
-  true if n == b
+  true if n == b && a != b
 end
 
-puts (1..10000).select{|i| i if has_pair?(i)}.inject(:+)
+puts (1..10000).select{|i| i if i.even? && has_pair?(i)}.inject(:+)
+puts "Elapsed time: " + (Time.now - time).to_s
