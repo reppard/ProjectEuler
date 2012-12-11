8bc773adf6371c8a75c369471c12c18a2f26f05b
reppardwalker <rwalker@reppardwalkers-MacBook-Air.local>
2012-12-11 16:29:32 -0500

    Added readme and fixed commented typos in problem 11

--- problem_11.rb
+++ problem_11.rb
@@ -1,8 +1,8 @@
-#In the 2020 grid below, four numbers along a diagonal line have been marked in red.
+#In the 20x20 grid below, four numbers along a diagonal line have been marked in red.
 #The product of these numbers is 26  63  78  14 = 1788696.
 
 #What is the greatest product of four adjacent numbers in any direction
-#(up, down, left, right, or diagonally) in the 2020 grid?
+#(up, down, left, right, or diagonally) in the 20x20 grid?
 
 time = Time.now
 
