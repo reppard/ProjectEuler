6277e593ef9de4f5d6e6a927df3e3f4e1695a593
reppardwalker <rwalker@reppardwalkers-MacBook-Air.local>
2012-12-12 09:32:27 -0500

    Still solves problem but trying to get spec to run with smaller grid

--- spec/problem_11_spec.rb
+++ spec/problem_11_spec.rb
@@ -4,26 +4,26 @@ describe ProblemEleven do
 
   before(:each) do
     @foo = ProblemEleven.new
-    @nums = @foo.instance_variable_get(:@nums)
-  end
-
-  it "should exist" do
-    true
-  end
-
-  it "nums should be array" do
-    @nums.class.should eq(Array)
+    @grid = "11 01 01 01 01 01 01 01
+             01 01 01 01 01 01 01 01
+             01 01 01 01 01 01 01 01
+             01 11 01 31 01 01 01 01
+             01 01 01 01 01 01 01 01
+             01 01 01 01 01 01 01 01
+             01 01 01 01 01 01 01 01
+             01 01 01 01 01 01 01 01"
   end
 
   it "setup_grid should return array" do
-    @foo.setup_grid.should be_kind_of(Array)
+    @foo.setup_grid(@grid).should be_kind_of(Array)
   end
 
   describe "find max prod" do
 
     it "should return the max product for all rows and columns" do
-      grid = @foo.setup_grid
-      @foo.find_max_prod(grid).should eq(51267216)
+      grid = @foo.setup_grid(@grid)
+      @foo.find_rows_and_cols_max_prod(grid).should eq(11)
+      @foo.find_diag_max_prod(grid).should eq(31)
     end
 
   end
