603219c011773281eb376e241379d5100d9a0721
reppardwalker <rwalker@reppardwalkers-MacBook-Air.local>
2013-02-21 18:21:57 -0500

    Implemented month hash
    Not real sure what I was doing here as it was a while ago.  This problem
    hasn't been completed obviously.  Will need to revisit.

--- problem_19.rb
+++ problem_19.rb
@@ -4,10 +4,14 @@ end
 
 
 class Years
-  attr_accessor :range
+  attr_accessor :range, :year
 
   def initialize range
-    @range = range.to_a
+    if range.class == Range
+      @range = range.to_a
+    else
+      @range = range
+    end
   end
 
   def all
@@ -18,9 +22,43 @@ class Years
     true if year % 4 == 0 && year % 100 != 0
   end
 
+  class << self
+    def leap_year?
+      return self.year
+    end
+  end
 end
 
 
 class Months
+
+  MONTH_HASH = {
+  1     =>  31,
+  2     =>  28,
+  3     =>  31,
+  4     =>  30,
+  5     =>  31,
+  6     =>  30,
+  7     =>  31,
+  8     =>  31,
+  9     =>  30,
+  10    =>  31,
+  11    =>  30,
+  12    =>  31
+  }
+
+  attr_accessor :year, :current
+
+  def initialize year
+    @year = Years.new(year)
+  end
+
+  def [] number
+    @current = MONTH_HASH[number]
+  end
+
+  def number_of_days
+    @current
+  end
 
 end
