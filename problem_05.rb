9df0fb4c11425064ffded84309bb941d865d234f
reppardwalker <rwalker@reppardwalkers-MacBook-Air.local>
2013-02-21 18:18:45 -0500

    Refactored timing

--- problem_05.rb
+++ problem_05.rb
@@ -1,7 +1,7 @@
 #2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
- start = Time.new
+ time = Time.new
  range = [11,13,14,16,17,18,19,20]
  array = []
  num = 20
@@ -13,7 +13,6 @@
        num +=20
        array = []
    end
- finish = Time.new
- puts "Smallest positive number : " + num.to_s
- puts "Total Time : " + (finish - start).to_s + " seconds"
 
+ puts "Smallest positive number : " + num.to_s
+ puts "Total Time : #{Time.new - time} seconds"
