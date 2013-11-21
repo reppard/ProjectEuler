10ef46b8317839a70a45ec4cf15c914dec965453
reppard <reppardwalker@gmail.com>
2013-11-21 08:47:57 -0500

    Updated prime lib and problem 10 to use it

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,6 +1,6 @@
 def is_prime? num
-  collect = (2..Math.sqrt(num)).collect do |i|
-              !(num % i == 0 && i < num)
-            end
-  !collect.include?(false)
+  (2..Math.sqrt(num)).each do |i|
+    return false if (num % i == 0 && i < num)
+  end
+  true
 end
--- problem_10.rb
+++ problem_10.rb
@@ -1,28 +1,16 @@
 #Project 10
 #The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
 #Find the sum of all the primes below two million.
+require './lib/prime'
 time = Time.new
 sum = 0
-start = 3
+num = 3
 
-puts "Calculating..."
-
-def isPrime?(num)
-  (2..Math.sqrt(num)).each { |i|
-      if num % i == 0 && i < num
-        return false
-     end
-  }
-  true
-end
-
-while start < 2000000
-  pri = isPrime?(start)
-  if pri == true
-    sum += start
+while num < 2000000
+  if is_prime?(num)
+    sum += num
   end
-  start += 2
+  num += 2
 end
 puts "Answer : " + (sum + 2).to_s
 puts "Elapsed : " + (Time.new - time).to_s + " secs"
